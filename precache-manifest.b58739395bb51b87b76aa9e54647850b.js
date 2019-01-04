d4cb5fc5aaf9b91b2a1ad9609fc37b5c565feaa8
Krzysztof Ryszawy <krzysztof.ryszawy@gmail.com>
2019-01-04 17:54:33 +0100

    Updates

---
+++ precache-manifest.b58739395bb51b87b76aa9e54647850b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "48b4d817fc22e9ce4a83e0b6ddf6ab2e",
+    "url": "/krzysztofryszawy.github.io/static/media/React_travel_weather.48b4d817.jpg"
+  },
+  {
+    "revision": "a69cf64f5e0dc62a011c",
+    "url": "/krzysztofryszawy.github.io/static/css/main.50817fac.chunk.css"
+  },
+  {
+    "revision": "8e5f3952bf80154a2170",
+    "url": "/krzysztofryszawy.github.io/static/js/1.8e5f3952.chunk.js"
+  },
+  {
+    "revision": "f90df186df94342b3c32",
+    "url": "/krzysztofryszawy.github.io/static/js/runtime~main.f90df186.js"
+  },
+  {
+    "revision": "4d32db85dc931423cb70b29f7233aadf",
+    "url": "/krzysztofryszawy.github.io/static/media/header_back9.4d32db85.jpg"
+  },
+  {
+    "revision": "a995ccf5231dade7a09f4d786635ce4c",
+    "url": "/krzysztofryszawy.github.io/static/media/React_CarApp.a995ccf5.jpg"
+  },
+  {
+    "revision": "1129b20d7007629bcf03977a20076be0",
+    "url": "/krzysztofryszawy.github.io/static/media/React_StarWarsFlirt.1129b20d.jpg"
+  },
+  {
+    "revision": "9fdeedca64c3516c9172eb8f2b771744",
+    "url": "/krzysztofryszawy.github.io/static/media/React_business.9fdeedca.jpg"
+  },
+  {
+    "revision": "4db9f9a4821da791f2d596c19d953f86",
+    "url": "/krzysztofryszawy.github.io/static/media/React_calories.4db9f9a4.jpg"
+  },
+  {
+    "revision": "cf154609f2d6179a45d00cb3aace786d",
+    "url": "/krzysztofryszawy.github.io/static/media/React_reserved.cf154609.jpg"
+  },
+  {
+    "revision": "a69cf64f5e0dc62a011c",
+    "url": "/krzysztofryszawy.github.io/static/js/main.a69cf64f.chunk.js"
+  },
+  {
+    "revision": "306824e2787cc8dd0092b0d7282b82e9",
+    "url": "/krzysztofryszawy.github.io/static/media/X1_position-fixed.306824e2.jpg"
+  },
+  {
+    "revision": "7b4f0209168377ffec895ff2c59bf897",
+    "url": "/krzysztofryszawy.github.io/static/media/X2_media_agency.7b4f0209.jpg"
+  },
+  {
+    "revision": "c03126ddc286ea5c5a7a53b2763a7cf0",
+    "url": "/krzysztofryszawy.github.io/static/media/X3_Berliner_fest.c03126dd.jpg"
+  },
+  {
+    "revision": "c23cdea4a8f4f200f43512719e57491e",
+    "url": "/krzysztofryszawy.github.io/static/media/X5_toscana.c23cdea4.jpg"
+  },
+  {
+    "revision": "92ca98578047f360d3a78f0ee1002d18",
+    "url": "/krzysztofryszawy.github.io/static/media/X6_Berlin_tour.92ca9857.jpg"
+  },
+  {
+    "revision": "70f3844280f7e1cf451a93947b7ec985",
+    "url": "/krzysztofryszawy.github.io/static/media/X8_BMW.70f38442.jpg"
+  },
+  {
+    "revision": "41c061e8b1030526a67f86873299742c",
+    "url": "/krzysztofryszawy.github.io/static/media/project2.41c061e8.jpg"
+  },
+  {
+    "revision": "2864de907eeed47c199ffea73e96d73d",
+    "url": "/krzysztofryszawy.github.io/static/media/project3.2864de90.jpg"
+  },
+  {
+    "revision": "951a0d5dc569b1eb8cd43c9d119e6d3b",
+    "url": "/krzysztofryszawy.github.io/index.html"
+  }
+];
